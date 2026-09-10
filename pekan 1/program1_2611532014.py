afcf9d74d856993cf8b3a5ad3534f1ff6bd29b97
Subject9473 <amrinarasya6@gmail.com>
2026-09-10 23:57:14 +0700

    edit please

--- pekan 1/program1_2611532014.py
+++ pekan 1/program1_2611532014.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 #Program pertama
 """
 Komentar
@@ -10,17 +9,4 @@ print()
 print("Program ini menghasilkan")
 print(" / \\ // \\\\ /// \\\\\\")
 print("program ini " + "dibuat oleh \n" +
-=======
-#Program pertama
-"""
-Komentar
-dua baris atau lebih
-pertemuan 1 praktikum alpro
-"""
-print("Hello World")
-print()
-print("Program ini menghasilkan")
-print(" / \\ // \\\\ /// \\\\\\")
-print("program ini " + "dibuat oleh \n" +
->>>>>>> a8201781ba83e6bbdac5dcc32a8287dea8392894
       "Amrina Rasyada Yudhamy 2611532014")
