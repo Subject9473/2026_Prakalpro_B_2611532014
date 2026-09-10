a8201781ba83e6bbdac5dcc32a8287dea8392894
Subject9473 <amrinarasya6@gmail.com>
2026-09-10 23:13:54 +0700

    alpro1

---
+++ pekan 1/program1_2611532014.py
@@ -0,0 +1,12 @@
+#Program pertama
+"""
+Komentar
+dua baris atau lebih
+pertemuan 1 praktikum alpro
+"""
+print("Hello World")
+print()
+print("Program ini menghasilkan")
+print(" / \\ // \\\\ /// \\\\\\")
+print("program ini " + "dibuat oleh \n" +
+      "Amrina Rasyada Yudhamy 2611532014")
