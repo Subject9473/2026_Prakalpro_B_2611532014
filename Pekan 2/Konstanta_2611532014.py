e7e635cefcf722a48e12162df686d0b326f32871
Subject9473 <amrinarasya6@gmail.com>
2026-09-13 20:50:37 +0700

    fixing

--- Pekan 2/Konstanta_2611532014.py
+++ Pekan 2/Konstanta_2611532014.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 #buat file dengan nama konstanta_nim.py
 #program ini menggunakan konstanta untuk menghitung luas lingkaran
 #nama variabel ditambah 4 digit nim terakhir contoh: jari_2014
@@ -11,19 +10,3 @@ jari_2014 = float(input('masukkan nilai jari-jari: '))
 luas_2014 = PI * jari_2014 * jari_2014
 print("luas lingkaran dengan jari-jari %.2f adalah %.2f" % (jari_2014, luas_2014))
 
-
-=======
-#buat file dengan nama konstanta_nim.py
-#program ini menggunakan konstanta untuk menghitung luas lingkaran
-#nama variabel ditambah 4 digit nim terakhir contoh: jari_2014
-
-
-from typing import Final
-PI: Final = 3.14
-print("pi: %f" % (PI))
-jari_2014 = float(input('masukkan nilai jari-jari: '))
-luas_2014 = PI * jari_2014 * jari_2014
-print("luas lingkaran dengan jari-jari %.2f adalah %.2f" % (jari_2014, luas_2014))
-
-
->>>>>>> a8201781ba83e6bbdac5dcc32a8287dea8392894
